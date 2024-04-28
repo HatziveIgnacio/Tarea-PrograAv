9fb4a2800c14d5a24766d9608d3e6c41be18f096
Ignacio Layana <ig.layana.silva@gmail.com>
2024-04-27 22:31:22 -0400

    persona, getRegion

--- src/tarea/Persona.java
+++ src/tarea/Persona.java
@@ -1,30 +1,32 @@
-
 package tarea;
 
 public class Persona {
     private String nombre;
     private String rut;
     private String mesaVoto; // Mesa de votación a la que está asignada
+    private String region; // Nueva entrada para la región
 
-    // Método para establecer los datos de la persona
-    public void setPersona(String nombre, String rut, String mesaVoto) {
+    // Métodos para establecer y obtener los datos de la persona
+    public void setPersona(String nombre, String rut, String mesaVoto, String region) {
         this.nombre = nombre;
         this.rut = rut;
         this.mesaVoto = mesaVoto;
+        this.region = region;
     }
 
-    // Método para obtener el nombre de la persona
     public String getNombre() {
         return nombre;
     }
 
-    // Método para obtener el RUT de la persona
     public String getRut() {
         return rut;
     }
 
-    // Método para obtener la mesa de votación a la que está asignada la persona
     public String getMesa() {
         return mesaVoto;
+    }
+
+    public String getRegion() {
+        return region;
     }
 }
